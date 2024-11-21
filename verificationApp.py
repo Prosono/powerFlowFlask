1d15cb6619658e967c80a2a427c021cae9a557da
Prosono <norman576@hotmail.com>
2024-11-21 19:33:46 +0100

    update

--- verificationApp.py
+++ verificationApp.py
@@ -1,4 +1,4 @@
-from verificationApp import Flask, request, jsonify
+from flask import Flask, request, jsonify
 import requests
 import logging
 
@@ -73,4 +73,4 @@ def verify_subscription():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5002)
